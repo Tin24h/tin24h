22a8efd2dc1f9234285d1becda2fe67a42b4d343
Adminpc\Admin <diepnvph04430@fpt.edu.vn>
2016-11-24 11:31:00 +0700

    trang tin moi nhat

---
+++ app/src/main/java/tintuc/diepnvph04430/diep/tintuc/Tinmoinhat.java
@@ -0,0 +1,13 @@
+package tintuc.diepnvph04430.diep.tintuc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tinmoinhat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tinmoinhat);
+    }
+}
